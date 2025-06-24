ad76776cc934002dc56dea372ff0157a2cbf97c8
Yash Mehetre <mehetreyash12@gmail.com>
2025-06-24 20:22:33 +0530

    remove Placements from courses and create seperate components

--- src/App.jsx
+++ src/App.jsx
@@ -5,48 +5,50 @@ import Home from "./sections/Home";
 import Courses from "./sections/Courses";
 import About from "./sections/About";
 import Contact from "./sections/Contact";
-import Blog from "./pages/Blog"; 
+import Blog from "./pages/Blog";
 import Footer from "./components/Footer";
 import PopupForm from "./components/PopupForm";
-import ScrollToTop from "./components/ScrollToTop"; 
+import ScrollToTop from "./components/ScrollToTop";
 import CourseDetails from "./pages/CourseDetails";
-import PhoneCallButton from "./components/PhoneCallButton"; 
+import PhoneCallButton from "./components/PhoneCallButton";
 import WhatsAppButton from "./components/WhatsAppButton";
 import BlogCardSlider from "./sections/BlogCardSlider";
 import BlogDetails from "./pages/BlogDetails";
 import FloatingSocials from "./components/FloatingSocials";
 import CoursesPage from "./pages/CoursesPage";
 import CourseDetail from "./pages/CourseDetail";
+import PlacementHighlights from "./components/PlacementHighlights";
+
 const App = () => {
   return (
     <div>
       <Navbar />
       <Routes>
-        <Route 
-          path="/" 
+        <Route
+          path="/"
           element={
             <>
               <section id="home"><Home /></section>
+              <section id="placements"><PlacementHighlights /></section>
+
               <section id="courses"><Courses /></section>
               <section id="about"><About /></section>
-              <BlogCardSlider/>
+              <BlogCardSlider />
               <section id="contact"><Contact /></section>
-              
+
               <PopupForm />
               <ScrollToTop />
             </>
-          } 
+          }
         />
-         <Route path="/blog/:id" element={<Blog />} /> 
-        <Route path="/course/:id" element={<CourseDetails />} /> 
-        <Route path="/blogdetails" element={<BlogDetails />} /> 
+        <Route path="/blog/:id" element={<Blog />} />
+        <Route path="/course/:id" element={<CourseDetails />} />
+        <Route path="/blogdetails" element={<BlogDetails />} />
         <Route path="/courses" element={<CoursesPage />} />
         <Route path="/courses/:id" element={<CourseDetail />} />
       </Routes>
-      <Footer /> 
-      {/* <PhoneCallButton />  
-      <WhatsAppButton /> */}
-      <FloatingSocials/>
+      <Footer />
+      <FloatingSocials />
     </div>
   );
 };
